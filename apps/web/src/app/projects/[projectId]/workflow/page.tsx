89a287aec048cfc43926292c3c608b24d9bd9daf
Shafayat Saad <dev.saad30@gmail.com>
2026-07-19 20:22:32 +0600

    Create page.tsx

---
+++ apps/web/src/app/projects/[projectId]/workflow/page.tsx
@@ -0,0 +1,7 @@
+// Purpose: Render the project research jobs and execution timeline route.
+
+import { ResearchJobsPage } from "@/features/workflows/components/research-jobs-page";
+
+export default function ProjectWorkflowRoute() {
+  return <ResearchJobsPage />;
+}
