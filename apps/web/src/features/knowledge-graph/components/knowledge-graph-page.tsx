12bc4190d0b31f78cdb605e1300299eb98c70a3f
Shafayat Saad <dev.saad30@gmail.com>
2026-07-19 20:38:35 +0600

    Update knowledge-graph-page.tsx

--- apps/web/src/features/knowledge-graph/components/knowledge-graph-page.tsx
+++ apps/web/src/features/knowledge-graph/components/knowledge-graph-page.tsx
@@ -9,12 +9,12 @@ export function KnowledgeGraphPage() {
   return (
     <main className="relative min-h-screen overflow-y-auto bg-surface-container-lowest text-on-surface lg:h-screen lg:overflow-hidden">
       <div className="relative z-20 flex flex-col gap-4 p-5 pb-0 sm:p-8 sm:pb-0 lg:hidden">
-        <GraphSearch />
+        <GraphSearch searchId="graph-search-mobile" />
       </div>
       <GraphCanvas />
       <div className="relative z-20 flex flex-col gap-4 p-5 pt-0 sm:p-8 sm:pt-0 lg:block lg:min-h-screen lg:p-0">
         <div className="hidden lg:block">
-          <GraphSearch />
+          <GraphSearch searchId="graph-search-desktop" />
         </div>
         <GraphFilters />
         <NodeInspector />
