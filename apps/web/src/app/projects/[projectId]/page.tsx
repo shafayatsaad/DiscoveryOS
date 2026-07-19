5a315fe1893b96cd50bc5661544cb58b371e03ce
Shafayat Saad <dev.saad30@gmail.com>
2026-07-19 20:44:09 +0600

    Update page.tsx

--- apps/web/src/app/projects/[projectId]/page.tsx
+++ apps/web/src/app/projects/[projectId]/page.tsx
@@ -2,6 +2,12 @@
 
 import { ResearchProjectPage } from "@/features/projects/components/research-project-page";
 
-export default function ProjectRoute() {
-  return <ResearchProjectPage />;
+type ProjectRouteProps = {
+  params: Promise<{ projectId: string }>;
+};
+
+export default async function ProjectRoute({ params }: ProjectRouteProps) {
+  const { projectId } = await params;
+
+  return <ResearchProjectPage projectId={projectId} />;
 }
