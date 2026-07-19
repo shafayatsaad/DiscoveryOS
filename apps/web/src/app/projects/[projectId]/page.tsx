ac505ec6f2e00e0703bdde2f0c851072202788c3
Shafayat Saad <dev.saad30@gmail.com>
2026-07-19 19:40:11 +0600

    Create page.tsx

---
+++ apps/web/src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,7 @@
+// Purpose: Render an individual DiscoveryOS research project workspace.
+
+import { ResearchProjectPage } from "@/features/projects/components/research-project-page";
+
+export default function ProjectRoute() {
+  return <ResearchProjectPage />;
+}
