8e478ada80c8d10cda461077dc1329d28aa8f8b5
Shafayat Saad <dev.saad30@gmail.com>
2026-07-19 20:33:01 +0600

    Create page.tsx

---
+++ apps/web/src/app/projects/[projectId]/pipeline/page.tsx
@@ -0,0 +1,7 @@
+// Purpose: Render the project research pipeline execution route.
+
+import { ResearchJobsPage } from "@/features/workflows/components/research-jobs-page";
+
+export default function ProjectPipelineRoute() {
+  return <ResearchJobsPage />;
+}
