8688d27d052af914b013f7a276978a4b8049a9ee
Shafayat Saad <dev.saad30@gmail.com>
2026-07-19 19:57:50 +0600

    Create page.tsx

---
+++ apps/web/src/app/projects/[projectId]/graph/page.tsx
@@ -0,0 +1,7 @@
+// Purpose: Render the project knowledge graph explorer route.
+
+import { KnowledgeGraphPage } from "@/features/knowledge-graph/components/knowledge-graph-page";
+
+export default function ProjectKnowledgeGraphRoute() {
+  return <KnowledgeGraphPage />;
+}
